4a02b1507fbd71cc3ea8e508fc316e7f435704e8
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-25 01:02:52 -0700

    Initial Commit

---
+++ GenericType/ListInt.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericType
+{
+    public class CustomList<T>
+    {
+
+        private T[] items;
+        private int count;
+        public T capacity { get; set; }
+
+        public CustomList()
+        {
+            T[] nums = new T[0];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+
+            set
+            {
+                items[index] = value;
+            }
+        }
+
+        public void Add(T num)
+        {
+
+            Array.Resize(ref items, count + 1);
+            items[count] = num;
+            count++;
+        }
+
+        public void AddRange(params T[] NewItems)
+        {
+            foreach (var item in NewItems)
+            {
+                Add(item);
+
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += Convert.ToInt32(items[i]);
+            }
+            return sum;
+
+        }
+
+        public void Remove(T value)
+        {
+            T[] newList = new T[count];
+            int j = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i].Equals(value))
+                {
+                    newList[j] = items[i];
+                    j++;
+
+                }
+            }
+            items = newList;
+            count = j;
+
+
+        }
+
+        public void RemoveRange(params T[] NewItems)
+        {
+            foreach (var item in NewItems)
+            {
+                Remove(item);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", items);
+        }
+    }
+}
---
+++ GenericType/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> list = new CustomList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            //Console.WriteLine(list.Contains(1)); 
+            //list.Remove(5);
+            Console.WriteLine($"Sum:{list.Sum()}");
+            //Sum ve remove alinmir
+            Console.WriteLine(list);
+            list.AddRange(6, 7, 8);
+            Console.WriteLine(list);
+        }
+    }
+}
